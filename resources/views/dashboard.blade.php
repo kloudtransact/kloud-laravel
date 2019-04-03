5d297347a2b69f28b1241aa77012584d70a426da
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-03 12:39:09 +0100

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends("layout")
+
+@section('title',"Dashboard")
+
+@section('content')
+<div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <h2 class="section-heading">Dashboard</h2>
+            <hr class="my-4">
+          </div>
+</div>         
+@stop
