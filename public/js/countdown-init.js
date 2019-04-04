ff762b77b040d3c1fdfa47f6c0358b38cca9a3aa
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-04 17:07:50 +0100

    Add files via upload

---
+++ public/js/countdown-init.js
@@ -0,0 +1,44 @@
+(function() {
+	function pickElement(filter) {
+		return document.querySelector(filter);
+	}
+	
+	function pickElements(filter) {
+		return document.querySelectorAll(filter);
+	}
+
+	// Mayan Calendar: 1356088271111
+
+	
+	function update() {
+		var units = countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS,
+			
+			empty = "now!",
+			max = 11,
+			digits = 20;
+
+		var yyyy = 2019,
+			MM = 4,
+			dd = 5,
+			HH = 17,
+			mm = 59,
+			ss = 0,
+			fff = 0;
+
+		var start = new Date(yyyy, MM, dd, HH, mm, ss, fff),
+			ts = countdown(null, start, units, max, digits);
+
+		var deals = pickElements('.clockdiv.countdown span.deadline'),
+			//msg = ts.toHTML('strong', empty);
+			msg = ts.days + ":" + ts.hours + ":" + ts.minutes + ":" + ts.seconds;
+
+		deals.forEach(function(el) {
+         el.innerHTML = msg;
+       });
+       
+       var auctionSection = pickElement('#auction-section');
+
+		requestAnimationFrame(update, auctionSection);
+	}
+	update();
+})();
