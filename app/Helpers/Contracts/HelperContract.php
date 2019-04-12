1fcc01c9c46405930206f4fba1e3b84616d70755
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-12 15:02:46 +0100

    Add files via upload

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -6,6 +6,8 @@ Interface HelperContract
         public function sendEmail($to,$subject,$data,$view,$type);
         public function createUser($data);
         public function createShippingDetails($data);
-        public function createWallet($data);
+        public function createDeal($data);
+        public function createDealData($data);
+        public function createDealImage($data);
 }
  ?>
