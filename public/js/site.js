030de4a5a67889ba694fe42afcfb05192c0c54c7
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-16 11:21:12 +0100

    Add files via upload

--- public/js/site.js
+++ public/js/site.js
@@ -7,3 +7,35 @@ $('.shop-categories-linkk').click(function(e){
 	let c = $(this).attr('data-cat');
 	console.log("Category: " + c);
 });
+
+let x = getCookie('kloudtransact_gdpr');
+if (x) {
+    $('#cookieConsent').hide();
+}
+
+
+
+
+/********** Helper functions **********/
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
