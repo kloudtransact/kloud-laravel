2f1c0b7881f8c08741cfcf1a200e07da4dcbf2b1
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-16 11:56:05 +0100

    Add files via upload

---
+++ resources/views/admin-index.blade.php
@@ -0,0 +1,67 @@
+@extends("layout")
+
+@section('title',"Admin Dashboard")
+
+@section('content')
+<div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <h2 class="section-heading">Dashboard</h2>
+            <hr class="my-4">
+          </div>
+</div>         
+ <!-- Start Page Content -->
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-usd f-s-40 color-primary"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h2>&#8358;568120</h2>
+                                    <p class="m-b-0">Total Revenue</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-shopping-cart f-s-40 color-success"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h2>1178</h2>
+                                    <p class="m-b-0">Sales</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-archive f-s-40 color-warning"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h2>25</h2>
+                                    <p class="m-b-0">Deals</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-user f-s-40 color-danger"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h2>87</h2>
+                                    <p class="m-b-0">Users</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+@stop
---
+++ resources/views/transactions.blade.php
@@ -0,0 +1,12 @@
+@extends("layout")
+
+@section('title',"Transactions")
+
+@section('content')
+<div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <h2 class="section-heading">Transactions</h2>
+            <hr class="my-4">
+          </div>
+</div>         
+@stop
