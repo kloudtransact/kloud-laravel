745aa179424190efd242c0144768a6976a93b363
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-04-17 18:29:13 +0100

    Add files via upload

--- resources/views/deal.blade.php
+++ resources/views/deal.blade.php
@@ -16,27 +16,44 @@
                         </nav>
                     </div>
                 </div>
-
+                                     <?php
+                                      $images = $deal['images'];
+                                      shuffle($images);
+                                     $data = $deal['data'];
+                                    ?>
                 <div class="row">
                     <div class="col-12 col-lg-7">
                         <div class="single_product_thumb">
                             <div id="product_details_slider" class="carousel slide" data-ride="carousel">
                                 <ol class="carousel-indicators">
-                                    <li class="active" data-target="#product_details_slider" data-slide-to="0" style="background-image: url(img/product-img/pro-big-1.jpg);">
+                                	@if(count($images) > 0)
+                                      @for($a = 0; $a < count($images); $a++)
+                                       <?php
+                                         $image = $images[$a];
+                                         $active = ($a == 0) ? "class='active'" : "";
+                                       ?>
+                                       <li {{$active}} data-target="#product_details_slider" data-slide-to="{{$a}}" style="background-image: url({{$image['url']}});">
+                                       </li>                                    
+                                      @endfor
+                                    @else
+                                    <li data-target="#product_details_slider" data-slide-to="0" style="background-image: url(img/product-img/no-img.jpg);">
                                     </li>
-                                    <li data-target="#product_details_slider" data-slide-to="1" style="background-image: url(img/product-img/pro-big-2.jpg);">
-                                    </li>
-                                    <li data-target="#product_details_slider" data-slide-to="2" style="background-image: url(img/product-img/pro-big-3.jpg);">
-                                    </li>
-                                    <li data-target="#product_details_slider" data-slide-to="3" style="background-image: url(img/product-img/pro-big-4.jpg);">
-                                    </li>
+                                    @endif
                                 </ol>
                                 <div class="carousel-inner">
+                                	@if(count($images) > 0)
+                                      @for($a = 0; $a < count($images); $a++)
+                                       <?php
+                                         $image = $images[$a];
+                                         $active = ($a == 0) ? "class='active'" : "";
+                                       ?>
                                     <div class="carousel-item active">
-                                        <a class="gallery_img" href="img/product-img/pro-big-1.jpg">
-                                            <img class="d-block w-100" src="img/product-img/pro-big-1.jpg" alt="First slide">
+                                        <a class="gallery_img" href="{{$image['url']}}">
+                                            <img class="d-block w-100" src="{{$image['url']}}" alt="Slide {{$a + 1}}">
                                         </a>
                                     </div>
+                                    @endfor
+                                    @else
                                     <div class="carousel-item">
                                         <a class="gallery_img" href="img/product-img/pro-big-2.jpg">
                                             <img class="d-block w-100" src="img/product-img/pro-big-2.jpg" alt="Second slide">
@@ -52,6 +69,7 @@
                                             <img class="d-block w-100" src="img/product-img/pro-big-4.jpg" alt="Fourth slide">
                                         </a>
                                     </div>
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -61,29 +79,32 @@
                             <!-- Product Meta Data -->
                             <div class="product-meta-data">
                                 <div class="line"></div>
-                                <p class="product-price">$180</p>
-                                <a href="{{url('deal')}}">
-                                    <h6>White Modern Chair</h6>
+                                <p class="product-price">&#8358;{{$data['amount']}}</p>
+                                <a href="{{url('deal').'?sku='.$d['sku']}}">
+                                    <h6>{{$deal['name']}}</h6>
                                 </a>
                                 <!-- Ratings & Review -->
                                 <div class="ratings-review mb-15 d-flex align-items-center justify-content-between">
                                     <div class="ratings">
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                    	@for($s = 0; $s < $deal['rating']; $s++]
+                                          <i class="fa fa-star" aria-hidden="true"></i>
+                                        @endfor
                                     </div>
                                     <div class="review">
                                         <a href="#">Write A Review</a>
                                     </div>
                                 </div>
                                 <!-- Avaiable -->
-                                <p class="avaibility"><i class="fa fa-circle"></i> In Stock</p>
+                                <?php
+                                  $cl = "In Stock"; $ico = "fa-circle";
+                                  if($data['in_stock'] == "almost") $cl = "Almost Gone"; $ico = "fa-hourglass-half";
+                                  else if($data['in_stock'] == "no") $cl = "Out of Stock"; $ico = "fa-times-circle";
+                                 ?>
+                                <p class="avaibility"><i class="fa {{$ico}}"></i> {{$cl}}</p>
                             </div>
 
                             <div class="short_overview my-5">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quae eveniet culpa officia quidem mollitia impedit iste asperiores nisi reprehenderit consequatur, autem, nostrum pariatur enim?</p>
+                                {!! $data['description']!!}
                             </div>
 
                             <!-- Add to Cart Form -->
@@ -96,9 +117,14 @@
                                         <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-caret-up" aria-hidden="true"></i></span>
                                     </div>
                                 </div>
-                                <button id="" name="addtocart" value="5" class="btn amado-btn">Place bid</button>
-                                <button id="" name="addtocart" value="5" class="btn amado-btn active">Buy it now</button>
-                                <button id="" name="addtocart" value="5" class="btn amado-btn">Watch</button>
+                                <?php
+                                  $bidURL = url("bid")."?sku=".$deal['sku'];
+                                  $watchURL = url("watch")."?sku=".$deal['sku'];
+                                  $buyURL = url("buy")."?sku=".$deal['sku']."&qty=";
+                                 ?>
+                                <button id="" name="addtocart" value="5" onclick="window.location = '{{$bidURL}}';return false;" class="btn amado-btn">Place bid</button>
+                                <button id="" name="addtocart" value="5" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) window.location = '{{$buyURL}}' + qty;return false;" class="btn amado-btn active">Buy it now</button>
+                                <button id="" name="addtocart" value="5" onclick="window.location = '{{$watchURL}}';return false;" class="btn amado-btn">Watch</button>
                             </form>
 
                         </div>
