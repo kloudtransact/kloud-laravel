4f1d34fe8d618eb8675f65071650b8890047bcf3
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-01 02:37:39 +0100

    Add files via upload

--- resources/views/deal.blade.php
+++ resources/views/deal.blade.php
@@ -89,7 +89,7 @@
                                     	@for($s = 0; $s < $deal['rating']; $s++)
                                           <i class="fa fa-star" aria-hidden="true"></i>
                                         @endfor
-										@if($rated == "no")
+										@if(!$rated)
                                         <br>
                                         <h6>Rate this product:</h6>
                                         <select id="rating-select">
