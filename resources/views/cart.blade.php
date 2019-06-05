376a3c0476b12f2d937ed626ffc7eecea9a15d46
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-05 17:44:58 +0100

    Add files via upload

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -46,7 +46,7 @@
                                                 <p>Qty</p>
                                                 <div class="quantity">
                                                     <span class="qty-minus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 1 ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
-                                                    <input type="number" class="qty-text" id="qty" step="1" min="1" max="300" name="quantity" value="{{$c['qty']}}">
+                                                    <input type="number" class="qty-text" id="qty" step="1" min="1" max="300" name="quantity[]" value="{{$c['qty']}}">
                                                     <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
                                                 </div>
                                             </div>
