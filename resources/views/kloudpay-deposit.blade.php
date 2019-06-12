b479541e1854d13e545e9efa877b0ca1ed8108ee
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-12 17:49:06 +0100

    Add files via upload

--- resources/views/kloudpay-deposit.blade.php
+++ resources/views/kloudpay-deposit.blade.php
@@ -11,17 +11,32 @@
                             <div class="card">                        	     
                         	     <div class="card-body">
                         	       <h3 class="card-title" style="color: #fbb710 !important; padding: 5px;">Add funds to your KloudPay wallet</h3>                     
-                                   <form action="#" method="get">
-                                   	
+                                   <form action="" id="checkout-form" method="post">
+                                   	{!! csrf_field() !!}
                                 <div class="row">
                                     <div class="col-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-briefcase"></i> KloudPay: &#8358;{{$wallet['balance']}}</p><br>
-                                        <input type="number" class="form-control" name="amount" value="" placeholder="Enter amount" required><br>
+                                        <input type="number" class="form-control" name="orig-amount" id="amount" value="" placeholder="Enter amount" required><br>
                                         <p class="form-control-plaintext">Powered by <img class="img img-responsive" src="img/ps.jpg"/></p>
                                     </div>
-
+                                    <script>
+                             	let mc = {
+                             	                'type': 'kloudpay'
+                                             };
+                             
+                             </script>
+                            <!-- payment form -->
+                            	<input type="hidden" name="email" value="{{$user->email}}"> {{-- required --}}
+                            	<input type="hidden" name="amount" id="meta-amount" value=""> {{-- required in kobo --}}
+                            	<input type="hidden" name="metadata" id="nd" value="" > {{-- For other necessary things you want to add to your payload. it is optional though --}}
+                            	<input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+                           	 <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
+                            <!-- End payment form -->
+                            	
+                            <input type="hidden" id="card-action" value="{{url('pay')}}">
+                            	
                                     <div class="col-12">
-                                        <button type="submit" class="amado-btn">Make a deposit</button>                                                                           
+                                        <button type="submit" id="deposit-card" class="amado-btn">Make a deposit</button>                                                                           
                                     </div>
                                 </div>
                             </form>
