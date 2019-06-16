05a7721c557137dc5d59361780eaf54cd0c70a74
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-16 22:42:08 +0100

    Add files via upload

--- resources/views/admin/deals.blade.php
+++ resources/views/admin/deals.blade.php
@@ -33,126 +33,28 @@
                         </th>
                       </thead>
                       <tbody>
+                        @foreach($deals as $d)
                         <tr>
                           <td>
-                            KLO9873480233
+                            {{$d['sku']}}
                           </td>
                           <td>
-                            Toshiba 30-inch LED TV
+                            {{$d['name']}}
                           </td>
                           <td>
-                            @for($u = 0; $u < 4; $u++)
+                            @for($u = 0; $u < {{$d['rating']}}; $u++)
                             	<i class="material-icons text-primary">star</i>
                             @endfor
                           </td>
                           <td class="text-info">
-                           IN STOCK
+                           {{$d['data']['in_stock']}}
                           </td>
                           <td>
                            <a class="btn btn-success" href="{{url('cobra-deal')}}"> View</a>
                            <a class="btn btn-warning" href="#"> Delete</a>
                           </td>
                         </tr>
-                        <tr>
-                                                    <td>
-                            KLO0219858885
-                          </td>
-                          <td>
-                            HP Deskjet 2684
-                          </td>
-                          <td>
-                            @for($u = 0; $u < 3; $u++)
-                            	<i class="material-icons text-primary">star</i>
-                            @endfor
-                          </td>
-                          <td class="text-success">
-                           NEW
-                          </td>
-                          <td>
-                           <a class="btn btn-success" href=" {{url('cobra-deal')}}"> View</a>
-                           <a class="btn btn-warning" href="#"> Delete</a>
-                          </td>
-                        </tr>
-                        <tr>
-                                                    <td>
-                            KLO4887955854
-                          </td>
-                          <td>
-                            Samsung Galaxy S6
-                          </td>
-                          <td>
-                            <?php for($u = 0; $u < 2; $u++){ ?>
-                            	<i class="material-icons text-primary">star</i>
-                            <?php } ?>
-                          </td>
-                          <td class="text-info">
-                           IN STOCK
-                          </td>
-                          <td>
-                           <a class="btn btn-success" href=" {{url('cobra-deal')}}"> View</a>
-                           <a class="btn btn-warning" href="#"> Delete</a>
-                          </td>
-                        </tr>
-                        <tr>
-                                                    <td>
-                            KLO9647808513
-                          </td>
-                          <td>
-                            Samsung Galaxy S9 Edge
-                          </td>
-                          <td>
-                            <?php for($u = 0; $u < 4; $u++){ ?>
-                            	<i class="material-icons text-primary">star</i>
-                            <?php } ?>
-                          </td>
-                          <td class="text-info">
-                           IN STOCK
-                          </td>
-                          <td>
-                           <a class="btn btn-success" href=" {{url('cobra-deal')}}"> View</a>
-                           <a class="btn btn-warning" href="#"> Delete</a>
-                          </td>
-                        </tr>
-                        <tr>
-                                                    <td>
-                            KLO9764215779
-                          </td>
-                          <td>
-                            Apple iPhone 8 64GB
-                          </td>
-                          <td>
-                            <?php for($u = 0; $u < 5; $u++){ ?>
-                            	<i class="material-icons text-primary">star</i>
-                            <?php } ?>
-                          </td>
-                          <td class="text-success">
-                           NEW
-                          </td>
-                          <td>
-                           <a class="btn btn-success" href=" {{url('cobra-deal')}}"> View</a>
-                           <a class="btn btn-warning" href="#"> Delete</a>
-                          </td>
-                        </tr>
-                        <tr>
-                                                    <td>
-                            KLO56418948522
-                          </td>
-                          <td>
-                            HP Pavilion dv8
-                          </td>
-                          <td>
-                            @for($u = 0; $u < 3; $u++)
-                            	<i class="material-icons text-primary">star</i>
-                            @endfor
-                          </td>
-                          <td class="text-warning">
-                           OUT OF STOCK
-                          </td>
-                          <td>
-                           <a class="btn btn-success" href=" {{url('cobra-deal')}}"> View</a>
-                           <a class="btn btn-warning" href="#"> Delete</a>
-                          </td>
-                        </tr>
+                        @endforeach
                       </tbody>
                     </table>
                   </div><br>
