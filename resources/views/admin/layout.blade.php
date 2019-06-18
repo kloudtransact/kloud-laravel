2cc568ff463c3ebabee203e22b205bc4ed4af4bb
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-18 10:12:10 +0100

    Add files via upload

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,6 +1,49 @@
 @include("admin.head")
 @include("admin.sidebar")
-@include("admin.nav")       	
+@include("admin.nav")     
+
+<!--------- Session notifications-------------->
+        	<?php
+               $pop = ""; $val = "";
+               
+               if(session()->has("login-status"))
+               {
+               	$pop = "login-status"; $val = session()->get("login-status");
+               }
+               if(session()->has("cobra-deal-status"))
+               {
+               	$pop = "cobra-deal-status"; $val = session()->get("cobra-deal-status");
+               }
+               if(session()->has("reset-status"))
+               {
+               	$pop = "reset-status"; $val = session()->get("reset-status");
+               }
+               if(session()->has("add-deal-status"))
+               {
+               	$pop = "add-deal-status"; $val = session()->get("add-deal-status");
+               }
+               if(session()->has("remove-cart-status"))
+               {
+               	$pop = "remove-cart-status"; $val = session()->get("remove-cart-status");
+               }
+               if(session()->has("pay-card-status"))
+               {
+               	$pop = "pay-card-status"; $val = session()->get("pay-card-status");
+               }
+               if(session()->has("pay-kloudpay-status"))
+               {
+               	$pop = "pay-kloudpay-status"; $val = session()->get("pay-kloudpay-status");
+               }
+             ?> 
+
+                 @if($pop != "" && $val != "")
+                   @include('session-status',['pop' => $pop, 'val' => $val])
+                 @endif
+        	<!--------- Input errors -------------->
+                    @if (count($errors) > 0)
+                          @include('input-errors', ['errors'=>$errors])
+                     @endif 
+  	
 @yield("content")
 @include("admin.footer")
 @include("admin.foot")
