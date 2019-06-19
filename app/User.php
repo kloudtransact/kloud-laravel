2cf86ff908b01ce59f56625a1830c79a78ecb18c
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-19 13:11:19 +0100

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'fname', 'lname', 'phone', 'email', 'password', 'role', 'status',
+        'fname', 'lname', 'phone', 'email', 'password', 'role', 'status', 'reset_code'
     ];
 
     /**
