06fc7df919ce27bbcde55598de81d122d4d63f72
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-19 13:23:22 +0100

    Add files via upload

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -14,17 +14,17 @@
                {
                	$pop = "cobra-deal-status"; $val = session()->get("cobra-deal-status");
                }
-               if(session()->has("reset-status"))
+               if(session()->has("cobra-user-status"))
                {
-               	$pop = "reset-status"; $val = session()->get("reset-status");
+               	$pop = "cobra-user-status"; $val = session()->get("cobra-user-status");
                }
-               if(session()->has("add-deal-status"))
+               if(session()->has("cobra-forgot-password-status"))
                {
-               	$pop = "add-deal-status"; $val = session()->get("add-deal-status");
+               	$pop = "cobra-forgot-password-status"; $val = session()->get("cobra-forgot-password-status");
                }
-               if(session()->has("remove-cart-status"))
+               if(session()->has("reset-password-status"))
                {
-               	$pop = "remove-cart-status"; $val = session()->get("remove-cart-status");
+               	$pop = "reset-password-status"; $val = session()->get("reset-password-status");
                }
                if(session()->has("pay-card-status"))
                {
@@ -37,11 +37,15 @@
              ?> 
 
                  @if($pop != "" && $val != "")
+                   <div class='row'>
                    @include('session-status',['pop' => $pop, 'val' => $val])
+                   </div>
                  @endif
         	<!--------- Input errors -------------->
                     @if (count($errors) > 0)
+                          <div class='row'>
                           @include('input-errors', ['errors'=>$errors])
+                          </div>
                      @endif 
   	
 @yield("content")
