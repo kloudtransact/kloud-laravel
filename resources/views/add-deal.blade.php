0969b9f39191151fca895c946f9a56a9e8da39f3
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-07-13 17:22:14 +0100

    Add files via upload

--- resources/views/add-deal.blade.php
+++ resources/views/add-deal.blade.php
@@ -15,15 +15,15 @@
                                    <form action="{{url('add-deal')}}" method="post">
                                    	{!! csrf_field() !!}
                                 <div class="row">
-                                    <div class="col-6 mb-3">
+                                    <div class="col-6 col-xs-12 mb-3">
                                     	<p class="form-control-plaintext"><i class="fa fa-suitcase"></i> Name</p><br>
                                         <input type="text" class="form-control" placeholder="e. g Samsung Galaxy S9 Edge" name="name" required><br>
                                     </div>
-                                    <div class="col-6 mb-3">
+                                    <div class="col-6 col-xs-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-tag"></i> SKU</p><br>
                                         <input type="text" class="form-control" value="Will be generated automatically" disabled>
                                     </div>
-                                    <div class="col-7 mb-3">
+                                    <div class="col-7 col-xs-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-tags"></i> Category</p><br>
                                         <select class="form-control" name="category" required>
                                         	<option value="none">Select deal category</option>
@@ -34,13 +34,13 @@
                                        <p class="form-control-plaintext"><i class="fa fa-pencil"></i> Description</p><br>
                                        <textarea class="form-control" placeholder="Enter description" name="description" required></textarea>
                                     </div>
-                                    <div class="col-5 mb-3">
+                                    <div class="col-5 col-xs-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-briefcase"></i> Price (max. &#8358;100,000.00)</p><br>
                                         <input type="number" class="form-control" name="amount" id="amount" value="" placeholder="Enter amount" max="100000" required><br><br>
                                         <p class="form-control-plaintext"><i class="fa fa-picture-o"></i> Image URIs</p><br>
                                         <textarea name="images"  class="form-control" placeholder="Image URIs (http://website.com/my-image.png or images/my_image.png) separated by commas" required></textarea>
                                     </div>
-                                    <div class="col-6 mb-3">
+                                    <div class="col-6 col-xs-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-briefcase"></i> Inventory Status</p><br>
                                         <select class="form-control" name="in_stock">
                                      	  <option value="none">Select inventory status</option>
@@ -49,7 +49,7 @@
                                            <option value="out-of-stock">Out of Stock</option>
                                         </select>
                                     </div>
-                                    <div class="col-6 mb-3">
+                                    <div class="col-6 col-xs-12 mb-3">
                                         <p class="form-control-plaintext"><i class="fa fa-briefcase"></i> Status</p><br>
                                         <select class="form-control">
                           	            <option value="none">Select status</option>
@@ -58,7 +58,7 @@
                                         </select>
                                     </div>
                             	
-                                    <div class="col-12">
+                                    <div class="col-12 col-xs-12">
                                         <button type="submit" class="amado-btn">Submit</button>                                                                           
                                     </div>
                                 </div>
