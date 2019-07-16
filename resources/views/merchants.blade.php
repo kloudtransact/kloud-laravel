ec54297cc7ad03ae3f456146a3019449cab13ca7
ANonymous <postmaster.richmama@gmail.com>
2019-07-16 10:16:18 +0100

    merchants login and store

--- resources/views/merchants.blade.php
+++ resources/views/merchants.blade.php
@@ -1,14 +1,14 @@
 @extends("layout")
 
-@section('title',"KloudPay")
+@section('title',"Merchants")
 
 @section('content')
 <div class="row">
           <div class="col-lg-12 mx-auto text-center">
-            <h2 class="section-heading">KloudPay</h2>
+            <h2 class="section-heading">Merchants</h2>
             <hr class="my-4">
-            <h3><em>Shopping Made Easy!</em></h3>
-            <p>KloudPay is your online wallet where you have monetary value to spend on any product on the <strong class="text-primary">KloudTransact</strong> platform.</p>
+            <h3><em>Sell on KloudTransact</em></h3>
+            <p>Easily import your products and sell on the <strong class="text-primary">KloudTransact</strong> platform.</p>
             <br>
             <div class="row">
             	<div class="col-lg-4 col-md-6 col-sm-6">
@@ -16,8 +16,8 @@
          <div class="card-body">         
             <div class="text-primary">
             <h5 class="card-title"><i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></h5>
-            <p class="card-text">Buy Deals Easily</p>
-            <a href="{{url('top-deals')}}" class="btn btn-primary">Buy Top Deals Now</a>
+            <p class="card-text">Top Deals</p>
+            <a href="{{url('top-deals')}}" class="btn btn-primary">See top deals</a>
             </div>
          </div>
        </div>
@@ -27,8 +27,8 @@
          <div class="card-body">         
             <div class="text-warning">
             <h5 class="card-title"><i class="fa fa-money fa-2x" aria-hidden="true"></i></h5>
-            <p class="card-text">Send Funds To Your Friends</p>
-            <a href="{{url('kloudpay-transfer')}}" class="btn btn-primary">Transfer Funds</a>
+            <p class="card-text">Become a Merchant</p>
+            <a href="{{url('mregister')}}" class="btn btn-primary">Go</a>
             </div>
          </div>
        </div>
@@ -38,15 +38,15 @@
          <div class="card-body">         
             <div class="text-primary">
             <h5 class="card-title"><i class="fa fa-hourglass fa-2x" aria-hidden="true"></i></h5>
-            <p class="card-text">Bid on Auctions</p>
-            <a href="{{url('auctions')}}" class="btn btn-primary">Bid Now</a>
+            <p class="card-text">Login</p>
+            <a href="{{url('mlogin')}}" class="btn btn-primary">Login</a>
             </div>
          </div>
        </div>
             </div>
             </div>
             <br>
-            	<center><a href="{{url('wallet')}}" class="amado-btn">Go to your KloudPay Wallet</a></center>
           </div>
         </div>
+		@include('ad-space')
 @stop
